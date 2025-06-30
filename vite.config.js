bd804de1bb9729aea494e146d204c574b271963a
Mandar Lad <mandarlad007@gmail.com>
2025-06-30 16:14:01 +0530

    port update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/EmployeeTaskList/"
+  base: "/EmployeeTaskList/",
+  server  : {
+    port: 3001,
+    open: true,
+  },
 })
