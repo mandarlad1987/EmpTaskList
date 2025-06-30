8ee119137cb83807c0af8562c788bec251c1550e
Mandar Lad <mandarlad007@gmail.com>
2025-06-30 17:31:48 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/EmployeeTaskList/",
-  server  : {
-    port: 3001,
-    open: true,
-  },
-})
+  })
